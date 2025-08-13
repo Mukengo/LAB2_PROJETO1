0c5996fa7c568105469689c6dbe98fb79053e538
Mukego <andreluisfessinger@gmail.com>
2025-08-12 22:59:31 -0300

    Passo 3

--- lista1.py
+++ lista1.py
@@ -1,5 +1,17 @@
+import random
+
+
 def cumprimento(texto):
     return f"Olá, {texto}"
 
 
+def media_numeros_aleatorios():
+    numeros = [random.randint(1, 100) for _ in range(7)]
+    media = sum(numeros) / len(numeros)
+    return media
+
+
 print(cumprimento("André Luís França Essinger"))
+
+media = media_numeros_aleatorios()
+print(f"A média dos 7 números sorteados é: {media:.2f}")
